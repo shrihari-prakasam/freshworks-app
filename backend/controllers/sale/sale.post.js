826ba7702eac9a4cba8b8311b284c544f6b2b75f
Shrihari Prakash <shrihariprakash@outlook.com>
2023-09-17 19:49:34 +0530

    Fixed missing customerId in sale creation.

--- backend/controllers/sale/sale.post.js
+++ backend/controllers/sale/sale.post.js
@@ -2,7 +2,7 @@ import SubscriptionSale from '../../model/subscription-sale.js';
 
 const POST__Sale = async (req, res) => {
     try {
-        const { customerName, subscriptionPlan, saleDate } = req.body;
+        const { customerId, customerName, subscriptionPlan, saleDate } = req.body;
         const newSale = new SubscriptionSale({ customerId, customerName, subscriptionPlan, saleDate });
         const savedSale = await newSale.save();
         res.json(savedSale);
--- backend/model/subscription-sale.js
+++ backend/model/subscription-sale.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const subscriptionSaleSchema = new mongoose.Schema({
+    customerId: String,
     customerName: String,
     subscriptionPlan: String,
     saleDate: Date,
